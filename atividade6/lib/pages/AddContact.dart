35c5481df92373b57a4102c1110fb4dc52c7fa9d
Pedro Vitor <pedrovtor8@gmail.com>
2025-05-15 22:46:15 -0300

    Versão 1

--- atividade6/lib/pages/AddContact.dart
+++ atividade6/lib/pages/AddContact.dart
@@ -8,8 +8,86 @@ class AddContact extends StatefulWidget {
 }
 
 class _AddContactState extends State<AddContact> {
+  List namesContacts = [];
+  List emailsContacts = [];
+  List phonesContacts = [];
+  TextEditingController controllerName = TextEditingController();
+  TextEditingController controllerEmail = TextEditingController();
+  TextEditingController controllerPhone = TextEditingController();
+
+  // Adicionar contato
+  void insertContact(String name, String email, String phone) {
+    setState(() {
+      namesContacts.add(name);
+      emailsContacts.add(email);
+      phonesContacts.add(email);
+    });
+    controllerName.clear();
+    controllerEmail.clear();
+    controllerPhone.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      //AppBar
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          'Adicionar Contato',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+            color: Colors.white,
+          ),
+        ),
+      ),
+
+      body: Container(
+        child: Column(
+          children: [
+            //Campo de texto contato
+            TextFormField(
+              controller: controllerName,
+              decoration: InputDecoration(
+                labelText: 'Digite o nome do contato',
+              ),
+            ),
+
+            //Campo de texto e-mail
+            TextFormField(
+              controller: controllerEmail,
+              decoration: InputDecoration(
+                labelText: 'Digite o e-mail do contato',
+              ),
+            ),
+
+            //Campo de texto telefone
+            TextFormField(
+              controller: controllerPhone,
+              decoration: InputDecoration(
+                labelText: 'Digite o telefone do contato',
+              ),
+            ),
+
+            TextButton(
+              onPressed: () {
+                String name = controllerName.text;
+                String email = controllerEmail.text;
+                String phone = controllerPhone.text;
+                insertContact(name, email, phone);
+
+                Navigator.pop(context, {
+                  'name': name,
+                  'email': email,
+                  'phone': phone,
+                });
+              },
+              child: Text("Salvar"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- atividade6/lib/pages/ContactList.dart
+++ atividade6/lib/pages/ContactList.dart
@@ -1,3 +1,4 @@
+import 'package:atividade6/pages/AddContact.dart';
 import 'package:flutter/material.dart';
 
 class ContactList extends StatefulWidget {
@@ -8,8 +9,57 @@ class ContactList extends StatefulWidget {
 }
 
 class _ContactListState extends State<ContactList> {
+  List<Map<String, String>> contacts = [];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      //AppBar
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          'Agenda de Contato',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            //Rota para a página AddContact
+            FloatingActionButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return AddContact();
+                    },
+                  ),
+                );
+              },
+              child: Icon(Icons.add),
+            ),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: contacts.length,
+                itemBuilder: (context, index) {
+                  final contact = contacts[index];
+                  return ListTile(
+                    title: Text(contact['name'] ?? ''),
+                    subtitle: Text('${contact['email']}\n${contact['phone']}'),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
